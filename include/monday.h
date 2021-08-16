b604a1cea96ff20e745fd4b2f13770b6eebb1375
maha7 <79159392+m-a-h-a@users.noreply.github.com>
2021-08-16 16:07:23 +0000

    Used try/catch to ensure user only enters valid number. Created output function which is void and could prob. return the value instead. Need to catch if the user enters anything other than 'c' or 'f' and fail gracefully

--- include/monday.h
+++ include/monday.h
@@ -6,31 +6,34 @@ float centigradeToFahrenheit(float c) {
   return ((c * 9 / 5) + 32); //Centigrade to Fahrenheit.
 }
 
+void output(float origTemp, float temperature, std::string convertTo, std::string convertFrom) {
+  std::cout << origTemp << " degrees " << convertFrom << " is " << temperature << " in " << convertTo << std::endl;
+}
 
 void fahrenheitCentigradeConversion(void) {
-  std::string userInput;
+  std::string userInput, tempChoice;
   float useableTemp = 0.0;
   float convertedTemp = 0.0;
 
   std::cout << std::endl << "Please enter the starting temperature: " << std::endl;
   std::getline(std::cin, userInput);
-  useableTemp = stof(userInput);
 
-  std::cout << "Press ‘C’ to convert from Fahrenheit to Centigrade" << std::endl;
-  std::cout << "Press ‘F’ to convert from Centigrade to Fahrenheit." << std::endl << std::endl;
+  try {
+    useableTemp = stof(userInput);
+    std::cout << std::endl << "Press ‘C’ to convert from Fahrenheit to Centigrade" << std::endl << "Press ‘F’ to convert from Centigrade to Fahrenheit." << std::endl << std::endl;
+    std::getline(std::cin, tempChoice);
 
-  std::cout << "\nYour choice: ";
-  std::getline(std::cin, userInput);
-
-  if(userInput == "C" || userInput == "c") {
-    convertedTemp = fahrenheitToCentigrade(useableTemp);
+    if (tempChoice == "C" || tempChoice == "c") {
+      convertedTemp = fahrenheitToCentigrade(useableTemp);
+      output(useableTemp, convertedTemp, "CENTIGRADE", "FAHRENHEIT");
+    }
+    if (tempChoice == "F" || tempChoice == "f") {
+      convertedTemp = centigradeToFahrenheit(useableTemp);
+      output(useableTemp, convertedTemp, "FAHRENHEIT", "CENTIGRADE");
+    }
+  } catch (std::exception e) {
+    std::cout << "Unrecognised entry. Please try again and ensure you only enter a number, e.g. 22 or 22.5" << std::endl;
   }
-
-  if(userInput == "F" || userInput == "f") {
-    convertedTemp = centigradeToFahrenheit(useableTemp);
-  }
-
-  std::cout << useableTemp << " degrees " << " is = " << convertedTemp << "\n";
 }
 
 void selfServiceCheckout(void) {
