da4a67905c3fbd4d6097263176a8214c5f134a3e
maha7 <79159392+m-a-h-a@users.noreply.github.com>
2021-08-19 08:21:19 +0000

    finishing off, adding comments, getting styling of the data file parser table to look as it's supposed to

--- include/wednesday.h
+++ include/wednesday.h
@@ -1,3 +1,9 @@
+//not ideal but create global variables (for data file parser)
+std::vector<std::string> itemsForDisplay;
+std::string maxSurname, maxSalaryLength = "";
+int intMaxSurnameSize, intMaxSalaryLength = 0;
+/*END*/
+
 bool fileExists(std::string filename) {
   //check if file exists
   struct stat buffer;
@@ -67,10 +73,31 @@ void phoneDirectory(void) {
   readFileFromSearch(fileName, userInput);
 }
 
+//from here down is "DATA FILE PARSER"
+void getSize(std::string textToSize, int colNo) {
+  if (colNo == 2) {
+    //looking at surnames
+    if (textToSize.size() > maxSurname.size()) {
+      //word we've been passed is larger
+      maxSurname = textToSize;
+    }
+    intMaxSurnameSize = maxSurname.size();
+  } else {
+    //looking at salaries
+    if (textToSize.size() > maxSalaryLength.size()) {
+      //salary we've been passed is longer
+      maxSalaryLength = textToSize;
+    }
+    intMaxSalaryLength = maxSalaryLength.size();
+  }
+}
+
 void splitByDelim(std::string lines, char delim) {
+  //I know, somewhere in my tiny brain, that there's a better way of doing this but my brain is fried
+  //I'm using flags to check if we want to add an item to col 1, 2 if not it's 3. it works, but I'm
+  //sure there's a better, more efficient way, it's just not coming to me at the mo.
   std::string buffer;
   std::string temp;
-  std::vector<std::string> itemsForDisplay;
   bool firstComma = true;
   bool secondComma = false;
 
@@ -84,33 +111,53 @@ void splitByDelim(std::string lines, char delim) {
     } else if (secondComma)  {
       //trim the whitespace at the start of the item
       buffer.erase(0, buffer.find_first_not_of(" \n\r\t"));
+      getSize(buffer, 2);
       itemsForDisplay.push_back(buffer); //push the item into a vector
       secondComma = false;
       firstComma = false;
     } else {
       buffer.erase(0, buffer.find_first_not_of(" \n\r\t"));
+      getSize(buffer, 3);
       itemsForDisplay.push_back(buffer); //push the item into a vector
       firstComma = true;
       secondComma = true;
     }
   }
+}
 
+void showDataFileParserResult(std::vector<std::string> displayItems) {
+  //we have the longest for each column, now we need to lay it out using setw.
+  //have +1 for the headers to get the extra space at the end but not on the "-"
+  //as this made it look odd.
+  std::cout << std::endl << std::setw(8) << "Initial " << std::left << std::setfill(' ')
+            << std::setw(intMaxSurnameSize+1) << "Last" << std::left << std::setfill(' ')
+            << std::setw(intMaxSalaryLength+1) << "Salary" << std::left << std::setfill(' ') << std::endl;
+  for (int i=0; i<7; i++) {
+    std::cout << std::setw(0) << "-";
+  }
+  std::cout << " "; //add a space
+  for (int i=0; i<(intMaxSurnameSize); i++) {
+    std::cout << "-";
+  }
+  std::cout << " ";
+  for (int i=0; i<(intMaxSalaryLength); i++) {
+    std::cout << "-";
+  }
+  std::cout << std::endl;
   for (int i=0; i<itemsForDisplay.size(); i++ ) {
-      std::cout << itemsForDisplay[i] << std::endl;
-    }
+    std::cout << std::setw(8) << std::left << std::setfill(' ') << itemsForDisplay[i];
+    i++;
+    std::cout << std::setw(intMaxSurnameSize+1) << std::left << std::setfill(' ') << itemsForDisplay[i];
+    i++;
+    std::cout << std::setw(intMaxSalaryLength+1) << std::left << std::setfill(' ') << itemsForDisplay[i]<< std::endl;
+  }
 }
 
 void splitFileByLine(std::string fileName) {
   bool doesFileExist = false;
   std::string fileLine;
   std::ifstream fileobject;
-  std::vector<std::string> items;
   char delim = ',';
-  std::vector<std::string> itemsForDisplay;
-  bool firstComma = true;
-  bool secondComma = false;
-  std::string temp;
-  std::string buffer;
 
   doesFileExist = fileExists(fileName); //check if file exists
 
@@ -123,9 +170,7 @@ void splitFileByLine(std::string fileName) {
     }
     fileobject.close(); //close the file
 
-    for (int i=0; i<itemsForDisplay.size(); i++ ) {
-      std::cout << itemsForDisplay[i] << std::endl;
-    }
+    showDataFileParserResult(itemsForDisplay); //display results
   }
 }
 
@@ -133,5 +178,4 @@ void dataFileParser(void) {
   std::string fileName = "primerChallenge6.csv";
 
   splitFileByLine(fileName);
-  
 }
