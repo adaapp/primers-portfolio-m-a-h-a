bd78f2772bfae35b23acafd6da7e0bfc94f5283c
maha7 <79159392+m-a-h-a@users.noreply.github.com>
2021-08-20 15:24:09 +0000

    Finishing off

--- include/friday.h
+++ include/friday.h
@@ -1,5 +1,6 @@
 class car {
   private:
+    //some defaults in case nothing is set
     const std::string DEFAULT_COLOUR = "Red";
     const std::string DEFAULT_MAKE = "Honda";
     const bool DEFAULT_ENGINE = 0;
@@ -10,7 +11,6 @@ class car {
     bool engine_turned_on;
     bool car_locked;
 
-    //if no params are passed, use these defaults
     void initialise_car(std::string colour, std::string make, bool running, bool is_locked) {
       set_colour(colour);
       set_make(make);
@@ -22,11 +22,10 @@ class car {
     car() {
       initialise_car(DEFAULT_COLOUR, DEFAULT_MAKE, DEFAULT_ENGINE, DEFAULT_LOCKED);
     }
+    //below allows a user to make a car of their own make and colour
     car(std::string make, std::string colour) {
       set_colour(colour);
       set_make(make);
-      engine_off();
-      locked(false);
     }
     
     void set_colour(std::string colour) { car_colour = colour; };
@@ -71,11 +70,7 @@ class car {
       engine_turned_on ? engine_label = "Running" : engine_label = "Off";
       car_locked ? locked_label = "Locked" : locked_label = "Unlocked";
       
-      std::cout << "Car Status: colour: " << car_colour << ", make: " << car_make << ", engine: " << engine_label << ", " << locked_label;
-    }      
-
-    ~car() {
-      //std::cout << std::endl << "destroyed" << std::endl;
+      std::cout << std::endl << "Car Status: colour: " << car_colour << ", make: " << car_make << ", engine: " << engine_label << ", " << locked_label;
     }
 };
 
@@ -93,12 +88,14 @@ void carMenu(car my_car) {
             << "Please select an option, or 0 to finish: " << std::endl;
     std::getline(std::cin, userInput);
 
+    //ensure we have an actual number
     try {
       intUserInput = stoi(userInput);
     } catch (std::exception e) {
-      std::cout << "try catch error" << std::endl;
+      std::cout << "Sorry, please only select from the menu above." << std::endl;
     }
 
+    //switch for the menu choices
     switch (intUserInput) {
       case 1:
         my_car.engine_on();
@@ -119,7 +116,7 @@ void carMenu(car my_car) {
 }
 
 void carClass(void) {
-  car my_car;
+  car my_car("Toyota", "Blue");
   carMenu(my_car);
 }
 
